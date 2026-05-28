f757a6b6e6d46739432b46b0bb9f5ad22421ccbd
Vin108 <122009737+Vin108@users.noreply.github.com>
2026-05-28 12:09:18 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,6 @@ android {
 }
 
 dependencies {
-    compileOnly("com.github.aniyomiorg:extensions-lib:1.5")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
