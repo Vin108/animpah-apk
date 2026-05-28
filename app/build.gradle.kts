04515b9b2e06bf83b4255c00c242dc5e293a4bd0
Vin108 <122009737+Vin108@users.noreply.github.com>
2026-05-28 11:32:42 +0530

    Add build.gradle.kts for Android application setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.serialization")
+}
+
+android {
+    namespace   = "eu.kanade.tachiyomi.animeextension.en.animepahe"
+    compileSdk  = 34
+
+    defaultConfig {
+        applicationId  = "eu.kanade.tachiyomi.animeextension.en.animepahe"
+        minSdk         = 24
+        targetSdk      = 34
+        versionCode    = 1
+        versionName    = "1.0"
+        manifestPlaceholders["tachiyomi.animeextension.class"] = "en.animepahe.AnimePahe"
+        manifestPlaceholders["tachiyomi.animeextension.name"]  = "AnimePahe"
+    }
+
+    buildTypes {
+        release { isMinifyEnabled = false }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
+
+dependencies {
+    compileOnly("com.github.aniyomiorg:extensions-lib:1.5")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("org.jsoup:jsoup:1.17.2")
+    implementation("androidx.preference:preference-ktx:1.2.1")
+}
